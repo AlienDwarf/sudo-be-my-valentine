92884dad98c1ab7cc31295474e4c8d2f2d5b0ea5
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2026-02-02 22:23:57 +0100

    gh pages setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Eternal Flame",
+    title: "sudo be my valentine",
     description: "A special question for a special someone.",
 };
 
